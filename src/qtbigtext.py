b1a0475a0fa7b77f759146a2fca2ba0407d6aeb3
Elliot Wolk <elliot.wolk@gmail.com>
2012-09-12 17:28:00 -0400

    sure is nice to have ctrl+c actually quit a fucking program

--- src/qtbigtext.py
+++ src/qtbigtext.py
@@ -9,12 +9,22 @@
 from PySide.QtGui import *
 
 import sys
+import signal
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 TYPEFACE= "Inconsolata"
 MIN_FONT_PT = 4
 MAX_FONT_PT = 600
 
 sampleText = ( ""
+  + "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod "
+  + "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim "
+  + "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea "
+  + "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate "
+  + "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint "
+  + "occaecat cupidatat non proident, sunt in culpa qui officia deserunt "
+  + "mollit anim id est laborum."
   + " `~!@#$%^&*()_ +-=[]\\{}|;':\",./<>?"
   + " abcdefghijkl mnopqrstuvwxyz"
   + " ABCDEFGHIJKL MNOPQRSTUVWXYZ"
