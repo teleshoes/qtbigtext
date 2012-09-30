643e942013322a4e06e21def2e012f34a55513f5
Elliot Wolk <elliot.wolk@gmail.com>
2012-09-30 14:39:26 -0400

    changed the sample text to something reasonable

--- src/qtbigtext.py
+++ src/qtbigtext.py
@@ -20,19 +20,7 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 CONF = os.getenv("HOME") + '/.config/qtbigtext.conf'
 
 sampleTextFile = os.getenv("HOME") + '/MyDocs/qtbigtext.txt'
-sampleText = ( ""
-  + "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod "
-  + "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim "
-  + "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea "
-  + "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate "
-  + "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint "
-  + "occaecat cupidatat non proident, sunt in culpa qui officia deserunt "
-  + "mollit anim id est laborum."
-  + " `~!@#$%^&*()_ +-=[]\\{}|;':\",./<>?"
-  + " abcdefghijkl mnopqrstuvwxyz"
-  + " ABCDEFGHIJKL MNOPQRSTUVWXYZ"
-  + " 0123456789"
-)
+sampleText = ("The quick brown fox jumped over the lazy dog.")
 
 name = sys.argv[0]
 usage = ("Usage:\n"
