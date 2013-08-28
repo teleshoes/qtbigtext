8c82066aa0727dcce492b0ff6beca32d14841b8a
Elliot Wolk <elliot.wolk@gmail.com>
2013-08-28 03:51:03 -0400

    update install script for debian, including package deps

--- install.pl
+++ install.pl
@@ -5,7 +5,10 @@ use warnings;
 my $binDest = "/usr/bin";
 my $libDest = "/opt/qtbigtext";
 
-system "rm", "-rf", $libDest;
-system "mkdir", "-p", $libDest;
-system "cp", "src/bigtext", $binDest;
-system "cp", "src/qtbigtext.py", $libDest;
+system "sudo", "apt-get", "install", qw(
+  python3-pyside python3-dbus
+);
+system "sudo", "rm", "-rf", $libDest;
+system "sudo", "mkdir", "-p", $libDest;
+system "sudo", "cp", "src/bigtext", $binDest;
+system "sudo", "cp", "src/qtbigtext.py", $libDest;
